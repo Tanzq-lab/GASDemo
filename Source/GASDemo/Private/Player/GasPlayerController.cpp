a94d52cf9cede08e11d7cd4386c8e5412a2f6df2
zhiqiang.tan <zhiqiang.tan@appshahe.com>
2023-10-30 15:25:03 +0800

    实现AI高亮

--- Source/GASDemo/Private/Player/GasPlayerController.cpp
+++ Source/GASDemo/Private/Player/GasPlayerController.cpp
@@ -2,3 +2,88 @@
 
 
 #include "Player/GasPlayerController.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystem/GasAbilitySystemComponent.h"
+#include "GASDemo/GASDemo.h"
+#include "Interaction/HighlightInterface.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+AGasPlayerController::AGasPlayerController()
+{
+	bReplicates = true;
+}
+
+void AGasPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+	// TODO  后续提取成一个 Ability
+	ReticleTrace();
+}
+
+UGasAbilitySystemComponent* AGasPlayerController::GetASC()
+{
+	if (GasAbilitySystemComponent == nullptr)
+	{
+		GasAbilitySystemComponent = Cast<UGasAbilitySystemComponent>(UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(GetPawn<APawn>()));
+	}
+	return GasAbilitySystemComponent;
+}
+
+void AGasPlayerController::HighlightActor(AActor* InActor)
+{
+	if (IsValid(InActor) && InActor->Implements<UHighlightInterface>())
+	{
+		IHighlightInterface::Execute_HighlightActor(InActor);
+	}
+}
+
+void AGasPlayerController::UnHighlightActor(AActor* InActor)
+{
+	if (IsValid(InActor) && InActor->Implements<UHighlightInterface>())
+	{
+		IHighlightInterface::Execute_UnHighlightActor(InActor);
+	}
+}
+
+void AGasPlayerController::ReticleTrace()
+{
+	// if (GetASC() && GetASC()->HasMatchingGameplayTag(FAuraGameplayTags::Get().Player_Block_CursorTrace))
+	// {
+	// 	UnHighlightActor(LastActor);
+	// 	UnHighlightActor(ThisActor);
+	// 	if (IsValid(ThisActor) && ThisActor->Implements<UHighlightInterface>())
+	//
+	// 		LastActor = nullptr;
+	// 	ThisActor = nullptr;
+	// 	return;
+	// }
+	const ECollisionChannel TraceChannel = /*IsValid(MagicCircle) ? ECC_ExcludePlayers : */ECC_Visibility;
+	auto World = GetWorld();
+	check(World);
+	if (UGameViewportClient* ViewportClient = World->GetGameViewport())
+	{
+		FVector2D OutSize;
+		ViewportClient->GetViewportSize(OutSize);
+		GetHitResultAtScreenPosition(OutSize / 2.f, TraceChannel, false, ReticleHit);
+	}
+	
+	if (!ReticleHit.bBlockingHit) return;
+
+	LastActor = ThisActor;
+	if (IsValid(ReticleHit.GetActor()) && ReticleHit.GetActor()->Implements<UHighlightInterface>())
+	{
+		ThisActor = ReticleHit.GetActor();
+	}
+	else
+	{
+		ThisActor = nullptr;
+	}
+
+	if (LastActor != ThisActor)
+	{
+		UnHighlightActor(LastActor);
+		HighlightActor(ThisActor);
+	}
+}
+
--- Source/GASDemo/Public/Player/GasPlayerController.h
+++ Source/GASDemo/Public/Player/GasPlayerController.h
@@ -6,6 +6,8 @@
 #include "GameFramework/PlayerController.h"
 #include "GasPlayerController.generated.h"
 
+class UGasAbilitySystemComponent;
+
 /**
  * 
  */
@@ -13,4 +15,33 @@ UCLASS()
 class GASDEMO_API AGasPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+public:
+	AGasPlayerController();
+
+	virtual void PlayerTick(float DeltaTime) override;
+
+private:
+
+	UPROPERTY()
+	TObjectPtr<UGasAbilitySystemComponent> GasAbilitySystemComponent;
+
+	UGasAbilitySystemComponent* GetASC();
+
+#pragma region Actor 高亮
+private:
+	UPROPERTY()
+	TObjectPtr<AActor> LastActor;
+	
+	UPROPERTY()
+	TObjectPtr<AActor> ThisActor;
+	
+	FHitResult ReticleHit;
+	
+	static void HighlightActor(AActor* InActor);
+	static void UnHighlightActor(AActor* InActor);
+	void ReticleTrace();
+
+#pragma endregion 
+	
 };
